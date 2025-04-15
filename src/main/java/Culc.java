20ad31f670ba554585600777c274ae68940f27ec
DESKTOP-8GHT8U8\NPC-71 <haru4m.work@gmail.com>
2025-04-15 16:43:28 +0900

    CSS訂正。input2タイトル直らず

--- src/main/java/Culc.java
+++ src/main/java/Culc.java
@@ -29,7 +29,7 @@ public class Culc extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		RequestDispatcher rd = request.getRequestDispatcher("imput2.html");
+		RequestDispatcher rd = request.getRequestDispatcher("input2.html");
 		rd.forward(request, response);
 	}
 
